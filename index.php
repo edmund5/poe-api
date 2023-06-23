d9d3dd96f6bc63dc0418d83436e52bf58724093f
Edmund Cinco <edmundcinco@me.com>
2023-06-23 21:23:12 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -2,15 +2,23 @@
 
 $lastContent = end(json_decode(file_get_contents("php://input"), true)['query'])['content'];
 
+header("HTTP/1.1 200 OK");
+header('Content-Type: text/event-stream');
+
 echo "event: meta\n";
 echo 'data: {"content_type": "text/markdown", "linkify": false, "suggested_replies": true}' . "\n\n";
+
 echo "event: text\n";
 echo 'data: {"text": "'.stripcslashes($lastContent).'"}' . "\n\n";
+
 echo "event: suggested_reply\n";
 echo 'data: {"text": "Hi"}' . "\n\n";
+
 echo "event: suggested_reply\n";
 echo 'data: {"text": "Hello"}' . "\n\n";
+
 echo "event: suggested_reply\n";
 echo 'data: {"text": "Hey"}' . "\n\n";
+
 echo "event: done\n";
 echo 'data: {}' . "\n\n";
